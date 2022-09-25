d942462d92ef8c51f30e3ab255c46dd0394f9caf
nikachitava <chitava18.official@gmail.com>
2022-09-25 12:23:56 +0400

    classWork_1

---
+++ classWork_1/task_1/main.js
@@ -0,0 +1,10 @@
+let text = document.getElementById('text1')
+
+function setStyleText(string) {
+    text.style.fontWeight = 'bold'
+    text.style.textDecoration = 'underline'
+    text.style.fontStyle = 'italic'
+    text.innerText = string
+}
+
+setStyleText('hhheloasdqwe')
