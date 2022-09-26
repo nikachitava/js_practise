bfd8fe6927e3bd213e03b8b741086a00d6b2ed37
nikachitava <chitava18.official@gmail.com>
2022-09-26 11:37:39 +0400

    classWork_1_task_3

---
+++ classWork_2/lecture_2/task_3/script.js
@@ -0,0 +1,14 @@
+function createTable(width, height) {
+    document.write(`<table width=${width}px, height=${height}px> 
+        <tr>
+            <td>1</td>
+            <td>2</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>4</td>
+        </tr>
+    </table>`)
+}
+
+createTable(200, 100)
